0a5671f0412daf31a442c28ba5689b7196f7ea36
Wybren Terpstra <wterpstra@protonmail.com>
2024-07-30 17:16:05 +0200

    Added requirements.txt

--- clappform/__init__.py
+++ clappform/__init__.py
@@ -26,7 +26,7 @@ from .typedefs import GrpcChannelOptions, GrpcMetadata, RpcCallOptions
 from .utils import default_options
 
 # Metadata
-__version__ = "5.0.0-alpha3"
+__version__ = "5.0.0-alpha4"
 __author__ = "Clappform B.V."
 __email__ = "info@clappform.com"
 __license__ = "MIT"
